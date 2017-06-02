50433b2672a42240882209599c4ef5b1aa1483d5
Ngan Nguyen <ngannguyen977@gmail.com>
2017-06-02 16:58:09 +0700

    lession 24

--- src/app/main/role/role.component.ts
+++ src/app/main/role/role.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
-import { NotificationService } from '../../core/services/notification.service'
+import { NotificationService } from '../../core/services/notification.service';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { MessageConstants } from '../../core/common/message.constants';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-role',
@@ -8,14 +11,19 @@ import { NotificationService } from '../../core/services/notification.service'
   styleUrls: ['./role.component.css']
 })
 export class RoleComponent implements OnInit {
+  //dùng ViewChild để tương tác với bên ngoài
+  //khai báo ViewChild với tham số là id của modalAddEdit (#modalAddEdit) của popup
+  @ViewChild('modalAddEdit') public modalAddEdit: ModalDirective;
 
-  constructor(private _dataService: DataService) { }
   public pageIndex: number = 1;
-  public pageSize: number = 2;
+  public pageSize: number = 20;
   public pageDisplay: number = 10;
   public filter: string = '';
   public roles: any;
   public totalRow: number;
+  public entity: any;
+
+  constructor(private _dataService: DataService, private _notificationService: NotificationService) { }
   ngOnInit() {
     this.loadData();
   }
@@ -28,8 +36,58 @@ export class RoleComponent implements OnInit {
         this.totalRow = response.TotalRows;
       });
   }
-  pageChanged(event: any):void{
+  //viet ham load DB
+  loadRole(id: any) {
+    this._dataService.get('/api/appRole/detail/' + id)
+      .subscribe((response: any) => {
+        this.entity = response;
+        console.log(this.entity);
+      });
+  }
+  pageChanged(event: any): void {
     this.pageIndex = event.page;
     this.loadData();
+  }
+  showAddModal() {
+    this.entity = {};
+    this.modalAddEdit.show();
+  }
+  showEditModal(id) {
+    this.loadRole(id);
+    this.modalAddEdit.show();
+
+  }
+  saveChange(valid: boolean) {
+    if (valid) {
+      if (this.entity.Id == undefined) {
+        this._dataService.post('/api/appRole/add', JSON.stringify(this.entity))
+          .subscribe((response: any) => {
+            console.log(response);
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageConstants.CREATE_OK_MSG);
+          }, error => this._dataService.handleError(error));
+      }
+      //nếu Id != undefined, nghĩa là Id có giá trị => update
+      else {
+        this._dataService.put('/api/appRole/update', JSON.stringify(this.entity))
+          .subscribe((response: any) => {
+            console.log(response);
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageConstants.UPDATE_OK_MSG);
+          }, error => this._dataService.handleError(error));
+      }
+    }
+  }
+  deleteItem(id: any) {
+    this._notificationService.printConfirmationDialog(MessageConstants.CONFIRM_DELETA_MSG, () => this.deleteItemConfirm(id));
+  }
+  deleteItemConfirm(id: any) {
+    this._dataService.delete('/api/appRole/delete', 'id', id)
+      .subscribe((response:Response) => {
+        this._notificationService.printSuccessMessage(MessageConstants.DELETE_OK_MSG);
+        this.loadData();
+      })
   }
 }
--- src/app/main/role/role.module.ts
+++ src/app/main/role/role.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DataService } from '../../core/services/data.service';
 import { NotificationService } from '../../core/services/notification.service';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { FormsModule } from '@angular/forms';
 
 const roleRoutes: Routes = [
@@ -16,7 +17,9 @@ const roleRoutes: Routes = [
     CommonModule,
     FormsModule,
     PaginationModule,
+    ModalModule.forRoot(),
     RouterModule.forChild(roleRoutes)
+    
   ],
   declarations: [RoleComponent],
   providers: [DataService, NotificationService]
